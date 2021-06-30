dd91dbb88b9b107fc62ce2f98ccb83ca7ceb2649
Boris Goranoff <boris.goranov.bg@gmail.com>
2021-06-30 20:25:35 +0300

    Feat: save username to shared preferences

--- app/src/main/java/com/github/bggoranoff/qchess/MainActivity.java
+++ app/src/main/java/com/github/bggoranoff/qchess/MainActivity.java
@@ -3,23 +3,30 @@ package com.github.bggoranoff.qchess;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.animation.ArgbEvaluator;
-import android.animation.ValueAnimator;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.TransitionDrawable;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.ViewAnimator;
 
 import com.github.bggoranoff.qchess.util.ChessAnimator;
 
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnKeyListener {
 
     private ConstraintLayout homeLayout;
     private TextView manualLink;
+    private EditText usernameEditText;
+    private Button playButton;
+    private ImageView iconView;
+    private SharedPreferences sharedPreferences;
 
     private void openManual(View view) {
         manualLink.animate().alpha(0.5f).setDuration(100);
@@ -28,16 +35,65 @@ public class MainActivity extends AppCompatActivity {
         manualLink.animate().alpha(1.0f);
     }
 
+    private void hideKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+    }
+
+    private void saveUsername() {
+        String username = usernameEditText.getText().toString();
+        sharedPreferences.edit().putString("username", username).apply();
+    }
+
+    private void play(View view) {
+        saveUsername();
+        // TODO: start play activity
+    }
+
+    private void chooseIcon(View view) {
+        saveUsername();
+        hideKeyboard(view);
+        // TODO: start icons activity
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(getSupportActionBar()).hide();
 
+        sharedPreferences = this.getSharedPreferences(
+                "com.github.bggoranoff.qchess",
+                Context.MODE_PRIVATE
+        );
+
         homeLayout = findViewById(R.id.homeLayout);
         ChessAnimator.animateBackground(homeLayout);
+        homeLayout.setOnClickListener(this::hideKeyboard);
+
+        usernameEditText = findViewById(R.id.usernameEditText);
+        usernameEditText.setText(
+                sharedPreferences.contains("username") ?
+                        sharedPreferences.getString("username", "") :
+                        ""
+        );
 
         manualLink = findViewById(R.id.manualLink);
         manualLink.setOnClickListener(this::openManual);
+
+        playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener(this::play);
+
+        iconView = findViewById(R.id.iconView);
+        iconView.setOnClickListener(this::chooseIcon);
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+            saveUsername();
+            hideKeyboard(v);
+        }
+        return false;
     }
 }
