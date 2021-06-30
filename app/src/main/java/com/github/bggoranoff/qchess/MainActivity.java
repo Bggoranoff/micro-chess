097094910c65d80a021e0425f8a5495329f6d9a7
Boris Goranoff <boris.goranov.bg@gmail.com>
2021-06-30 19:09:24 +0300

    Feat: color transition background animation

--- app/src/main/java/com/github/bggoranoff/qchess/MainActivity.java
+++ app/src/main/java/com/github/bggoranoff/qchess/MainActivity.java
@@ -1,15 +1,29 @@
 package com.github.bggoranoff.qchess;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.widget.ViewAnimator;
+
+import com.github.bggoranoff.qchess.util.ChessAnimator;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout homeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        homeLayout = findViewById(R.id.homeLayout);
+        ChessAnimator.animateBackground(homeLayout);
     }
 }
