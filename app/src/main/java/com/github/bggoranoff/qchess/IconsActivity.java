a6fdd2d735c7a7f2a50fe7d6dba3ea97ad3f4bd7
Boris Goranoff <boris.goranov.bg@gmail.com>
2021-07-01 20:37:27 +0300

    Feat: icons list layout

--- app/src/main/java/com/github/bggoranoff/qchess/IconsActivity.java
+++ app/src/main/java/com/github/bggoranoff/qchess/IconsActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class IconsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_icons);
+        Objects.requireNonNull(getSupportActionBar()).hide();
     }
 }
