1f538b32a7af2d88aea6a185820359a90de3a96c
Boris Goranoff <boris.goranov.bg@gmail.com>
2021-07-03 19:16:18 +0300

    Feat: redirect to games activity

--- app/src/main/java/com/github/bggoranoff/qchess/UserListActivity.java
+++ app/src/main/java/com/github/bggoranoff/qchess/UserListActivity.java
@@ -3,7 +3,10 @@ package com.github.bggoranoff.qchess;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.github.bggoranoff.qchess.util.ChessAnimator;
 
@@ -12,6 +15,12 @@ import java.util.Objects;
 public class UserListActivity extends AppCompatActivity {
 
     private ConstraintLayout layout;
+    private Button gamesButton;
+
+    private void redirectToGames(View view) {
+        Intent intent = new Intent(getApplicationContext(), GameListActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +30,8 @@ public class UserListActivity extends AppCompatActivity {
 
         layout = findViewById(R.id.userListLayout);
         ChessAnimator.animateBackground(layout);
+
+        gamesButton = findViewById(R.id.gamesButton);
+        gamesButton.setOnClickListener(this::redirectToGames);
     }
 }
