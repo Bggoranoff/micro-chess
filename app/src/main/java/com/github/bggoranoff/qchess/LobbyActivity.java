6dbe0333b6f9a6ee10a8d88bec5cc4c3b3d66bee
Boris Goranoff <boris.goranov.bg@gmail.com>
2021-07-08 15:49:04 +0300

    Feat: lobby activity layout

--- app/src/main/java/com/github/bggoranoff/qchess/LobbyActivity.java
+++ app/src/main/java/com/github/bggoranoff/qchess/LobbyActivity.java
@@ -1,18 +1,42 @@
 package com.github.bggoranoff.qchess;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.bggoranoff.qchess.util.ChessAnimator;
+
+import java.util.Objects;
+
 public class LobbyActivity extends AppCompatActivity {
+
+    private ConstraintLayout layout;
+    private TextView firstUserTextView;
+    private TextView secondUserTextView;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
+        layout = findViewById(R.id.lobbyLayout);
+        ChessAnimator.animateBackground(layout);
+
+        sharedPreferences = getSharedPreferences("com.github.bggoranoff.qchess", Context.MODE_PRIVATE);
         String opponentName = getIntent().getStringExtra("opponentName");
-        Toast.makeText(this, "Fight against " + opponentName + "!", Toast.LENGTH_SHORT).show();
+        String username = sharedPreferences.getString("username", "guest");
+
+        firstUserTextView = findViewById(R.id.firstUserName);
+        firstUserTextView.setText(username);
+
+        secondUserTextView = findViewById(R.id.secondUserName);
+        secondUserTextView.setText(opponentName);
     }
 }
