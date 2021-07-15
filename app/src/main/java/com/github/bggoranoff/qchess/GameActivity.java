b9f142ad70f8f85c10143e4eaac40c087dd47fca
Boris Goranoff <boris.goranov.bg@gmail.com>
2021-07-15 18:28:40 +0300

    Feat: game activity layout

--- app/src/main/java/com/github/bggoranoff/qchess/GameActivity.java
+++ app/src/main/java/com/github/bggoranoff/qchess/GameActivity.java
@@ -1,14 +1,25 @@
 package com.github.bggoranoff.qchess;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 
+import com.github.bggoranoff.qchess.util.ChessAnimator;
+
+import java.util.Objects;
+
 public class GameActivity extends AppCompatActivity {
+
+    ConstraintLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        layout = findViewById(R.id.gameLayout);
+        ChessAnimator.animateBackground(layout);
     }
 }
