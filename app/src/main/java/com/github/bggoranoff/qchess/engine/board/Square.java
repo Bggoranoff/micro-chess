060e4c0bcd29999d86c409d4fe06a79296e9de91
Boris Goranoff <boris.goranov.bg@gmail.com>
2021-07-19 08:37:32 +0300

    Feat: square class definition

--- app/src/main/java/com/github/bggoranoff/qchess/engine/board/Square.java
+++ app/src/main/java/com/github/bggoranoff/qchess/engine/board/Square.java
@@ -2,6 +2,11 @@ package com.github.bggoranoff.qchess.engine.board;
 
 import com.github.bggoranoff.qchess.engine.util.ChessColor;
 import com.github.bggoranoff.qchess.engine.piece.Piece;
+import com.github.bggoranoff.qchess.engine.util.Coordinates;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
 
 public class Square {
 
@@ -9,4 +14,42 @@ public class Square {
     private Piece piece = null;
     private String id;
     private String tag;
+    private Coordinates coordinates;
+
+    public Square(int x, int y) {
+        this.coordinates = new Coordinates(x, y);
+        color = (x + y) % 2 == 0 ? ChessColor.BLACK : ChessColor.WHITE;
+        tag = String.format(Locale.ENGLISH, "%c%d", (char) x + 97, y + 1);
+        id = String.format(Locale.ENGLISH, "cell%d%d", y, x);
+    }
+
+    public Square(int x, int y, Piece piece) {
+        this(x, y);
+        this.piece = piece;
+    }
+
+    public ChessColor getColor() {
+        return color;
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public @NotNull String toString() {
+        return tag;
+    }
 }
