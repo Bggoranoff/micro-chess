ebdc0b13d18e953acb0044679c993a00474506ee
Boris Goranoff <boris.goranov.bg@gmail.com>
2021-07-26 15:59:54 +0300

    Feat: past games list layout

--- app/src/main/java/com/github/bggoranoff/qchess/GameListActivity.java
+++ app/src/main/java/com/github/bggoranoff/qchess/GameListActivity.java
@@ -1,14 +1,25 @@
 package com.github.bggoranoff.qchess;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 
+import com.github.bggoranoff.qchess.util.ChessAnimator;
+
+import java.util.Objects;
+
 public class GameListActivity extends AppCompatActivity {
+
+    private ConstraintLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_list);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        layout = findViewById(R.id.gameListLayout);
+        ChessAnimator.animateBackground(layout);
     }
 }
